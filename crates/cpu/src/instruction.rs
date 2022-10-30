1ff99f0992189daa81eab98c2eb43df6b6832c74
takuya-katsurada <116815666+takuya-katsurada@users.noreply.github.com>
2022-10-31 00:06:13 +0900

    code(cpu): add instruction::from

--- crates/cpu/src/instruction.rs
+++ crates/cpu/src/instruction.rs
@@ -10,3 +10,12 @@ pub enum AddressingMode {
 
 #[derive(Copy, Clone, Debug)]
 pub struct Instruction(Opcode, AddressingMode);
+
+impl Instruction {
+    pub fn from(opcode: u8) -> Instruction {
+        match opcode {
+            0x29 => Instruction(Opcode::AND, AddressingMode::Immediate),
+            _ => panic!("unsupported CPU instruction:{:08x}", opcode),
+        }
+    }
+}
