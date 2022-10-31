3f7b4bcb16a38ba38126d236cb797a3d5aee8e2f
takuya-katsurada <116815666+takuya-katsurada@users.noreply.github.com>
2022-10-31 11:06:26 +0900

    code(cpu): several addressing modes have been added

--- crates/cpu/src/instruction.rs
+++ crates/cpu/src/instruction.rs
@@ -5,7 +5,19 @@ pub enum Opcode {
 
 #[derive(PartialEq, Eq, Copy, Clone, Debug)]
 pub enum AddressingMode {
-    Immediate
+    Implied,
+    Accumulator,
+    Immediate,
+    ZeroPage,
+    ZeroPageX,
+    ZeroPageY,
+    Absolute,
+    AbsoluteX,
+    AbsoluteY,
+    Indirect,
+    IndirectX,
+    IndirectY,
+    Relative,
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -14,7 +26,14 @@ pub struct Instruction(Opcode, AddressingMode);
 impl Instruction {
     pub fn from(opcode: u8) -> Instruction {
         match opcode {
+            0x21 => Instruction(Opcode::AND, AddressingMode::IndirectX),
+            0x25 => Instruction(Opcode::AND, AddressingMode::ZeroPage),
             0x29 => Instruction(Opcode::AND, AddressingMode::Immediate),
+            0x2d => Instruction(Opcode::AND, AddressingMode::Absolute),
+            0x31 => Instruction(Opcode::AND, AddressingMode::IndirectY),
+            0x35 => Instruction(Opcode::AND, AddressingMode::ZeroPageX),
+            0x39 => Instruction(Opcode::AND, AddressingMode::AbsoluteY),
+            0x3d => Instruction(Opcode::AND, AddressingMode::AbsoluteX),
             _ => panic!("unsupported CPU instruction:{:08x}", opcode),
         }
     }
