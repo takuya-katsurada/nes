5fec08bd7a05b785c845028bcdb4775323a9fc01
takuya-katsurada <116815666+takuya-katsurada@users.noreply.github.com>
2022-11-03 01:48:16 +0900

    code(cpu): add Cpu::fetch_u8

--- crates/cpu/src/lib.rs
+++ crates/cpu/src/lib.rs
@@ -1,5 +1,8 @@
 mod instruction;
 
+use memory::system::SystemBus;
+use memory::Memory;
+
 #[derive(Clone)]
 pub struct Cpu {
     // Accumulator
@@ -26,5 +29,30 @@ impl Default for Cpu {
             pc: 0,
             sp: 0,
         }
+    }
+}
+
+impl Cpu {
+    fn fetch_u8(&mut self, system: &mut memory::system::SystemBus) -> u8 {
+        let v = system.read_u8(self.pc);
+        self.pc += 1;
+        v
+    }
+}
+
+mod tests {
+    use memory::system::SystemBus;
+
+    # [test]
+    fn test_fetch_u8() {
+        let mut cpu = super::Cpu::default();
+        let mut mem = memory::Memory::default();
+
+        cpu.pc = 0x0002u16;
+        mem.write_u8(0x0002u16, 0xf0u8);
+
+        let v = cpu.fetch_u8(&mut mem);
+        assert_eq!(v, 0xf0u8);
+        assert_eq!(cpu.pc, 0x0003u16);
     }
 }
