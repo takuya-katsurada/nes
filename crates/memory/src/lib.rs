4a41f0292301990ef4505dd72b5c46dd9dec11ea
takuya-katsurada <116815666+takuya-katsurada@users.noreply.github.com>
2022-11-03 00:31:08 +0900

    code(mem): add memory::Memory

--- crates/memory/src/lib.rs
+++ crates/memory/src/lib.rs
@@ -1,7 +1,6 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+pub const RAM_SIZE: usize = 0x0800;
+
+#[derive(Clone)]
+pub struct Memory {
+    pub ram: [u8; RAM_SIZE]
 }
