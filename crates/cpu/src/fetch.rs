8e66f1b9ef24a66724e862548654b2bcc8dfdaee
takuya-katsurada <116815666+takuya-katsurada@users.noreply.github.com>
2022-11-03 23:41:29 +0900

    code(cpu): add fetch.rs

---
+++ crates/cpu/src/fetch.rs
@@ -0,0 +1,26 @@
+use crate::Cpu;
+
+impl Cpu {
+    fn fetch_u8(&mut self, system: &mut memory::system::SystemBus) -> u8 {
+        let v = system.read_u8(self.pc);
+        self.pc += 1;
+        v
+    }
+}
+
+mod tests {
+    use memory::system::SystemBus;
+
+    # [test]
+    fn test_fetch_u8() {
+        let mut cpu = super::Cpu::default();
+        let mut mem = memory::Memory::default();
+
+        cpu.pc = 0x0002u16;
+        mem.write_u8(0x0002u16, 0xf0u8);
+
+        let v = cpu.fetch_u8(&mut mem);
+        assert_eq!(v, 0xf0u8);
+        assert_eq!(cpu.pc, 0x0003u16);
+    }
+}
--- crates/cpu/src/lib.rs
+++ crates/cpu/src/lib.rs
@@ -1,7 +1,8 @@
+mod fetch;
 mod instruction;
 
-use memory::system::SystemBus;
 use memory::Memory;
+use memory::system::SystemBus;
 
 #[derive(Clone)]
 pub struct Cpu {
@@ -29,30 +30,5 @@ impl Default for Cpu {
             pc: 0,
             sp: 0,
         }
-    }
-}
-
-impl Cpu {
-    fn fetch_u8(&mut self, system: &mut memory::system::SystemBus) -> u8 {
-        let v = system.read_u8(self.pc);
-        self.pc += 1;
-        v
-    }
-}
-
-mod tests {
-    use memory::system::SystemBus;
-
-    # [test]
-    fn test_fetch_u8() {
-        let mut cpu = super::Cpu::default();
-        let mut mem = memory::Memory::default();
-
-        cpu.pc = 0x0002u16;
-        mem.write_u8(0x0002u16, 0xf0u8);
-
-        let v = cpu.fetch_u8(&mut mem);
-        assert_eq!(v, 0xf0u8);
-        assert_eq!(cpu.pc, 0x0003u16);
     }
 }
