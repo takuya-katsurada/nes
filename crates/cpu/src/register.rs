91771355dcc26c75c13a281a39ccbd6884c450c6
takuya-katsurada <116815666+takuya-katsurada@users.noreply.github.com>
2022-11-06 16:28:47 +0900

    code(cpu): implement read and write to some registers

--- crates/cpu/src/register.rs
+++ crates/cpu/src/register.rs
@@ -1,8 +1,13 @@
 use crate::Cpu;
 
-pub const CARRY_FLAG: u8 = 0x01u8;
+pub const CARRY_FLAG:     u8 = 0x01u8;
+pub const ZERO_FLAG:      u8 = 0x02u8;
 pub const INTERRUPT_FLAG: u8 = 0x04u8;
-pub const DECIMAL_FLAG: u8 = 0x08u8;
+pub const DECIMAL_FLAG:   u8 = 0x08u8;
+pub const BREAK_FLAG:     u8 = 0x10u8;
+pub const RESERVED_FLAG:  u8 = 0x20u8;
+pub const OVERFLOW_FLAG:  u8 = 0x40u8;
+pub const NEGATIVE_FLAG:  u8 = 0x80u8;
 
 impl Cpu {
     #[inline(always)]
@@ -12,6 +17,15 @@ impl Cpu {
     #[inline(always)]
     pub(crate) fn read_carry_flag(&mut self) -> bool {
         self.read_status_flag(CARRY_FLAG)
+    }
+
+    #[inline(always)]
+    pub(crate) fn write_zero_flag(&mut self, is_active: bool) {
+        self.write_status_flag(ZERO_FLAG, is_active);
+    }
+    #[inline(always)]
+    pub(crate) fn read_zero_flag(&mut self) -> bool {
+        self.read_status_flag(ZERO_FLAG)
     }
 
     #[inline(always)]
@@ -30,6 +44,42 @@ impl Cpu {
     #[inline(always)]
     pub(crate) fn read_decimal_flag(&mut self) -> bool {
         self.read_status_flag(DECIMAL_FLAG)
+    }
+
+    #[inline(always)]
+    pub(crate) fn write_break_flag(&mut self, is_active: bool) {
+        self.write_status_flag(BREAK_FLAG, is_active);
+    }
+    #[inline(always)]
+    pub(crate) fn read_break_flag(&mut self) -> bool {
+        self.read_status_flag(BREAK_FLAG)
+    }
+
+    #[inline(always)]
+    pub(crate) fn write_reserved_flag(&mut self, is_active: bool) {
+        self.write_status_flag(RESERVED_FLAG, is_active);
+    }
+    #[inline(always)]
+    pub(crate) fn read_reserved_flag(&mut self) -> bool {
+        self.read_status_flag(RESERVED_FLAG)
+    }
+
+    #[inline(always)]
+    pub(crate) fn write_overflow_flag(&mut self, is_active: bool) {
+        self.write_status_flag(OVERFLOW_FLAG, is_active);
+    }
+    #[inline(always)]
+    pub(crate) fn read_overflow_flag(&mut self) -> bool {
+        self.read_status_flag(OVERFLOW_FLAG)
+    }
+
+    #[inline(always)]
+    pub(crate) fn write_negative_flag(&mut self, is_active: bool) {
+        self.write_status_flag( NEGATIVE_FLAG, is_active);
+    }
+    #[inline(always)]
+    pub(crate) fn read_negative_flag(&mut self) -> bool {
+        self.read_status_flag(NEGATIVE_FLAG)
     }
 
     fn read_status_flag(&mut self, status: u8) -> bool {
