4b15a9f5194279f4d0b9f10ea78c38a27bce0e4d
takuya-katsurada <116815666+takuya-katsurada@users.noreply.github.com>
2022-11-10 22:57:41 +0900

    code(cpu): add Cpu::fetch_u16

--- crates/cpu/src/fetch.rs
+++ crates/cpu/src/fetch.rs
@@ -6,6 +6,12 @@ impl Cpu {
         self.pc += 1;
         v
     }
+
+    pub(crate) fn fetch_u16(&mut self, system: &mut memory::system::SystemBus) -> u16 {
+        let lo = self.fetch_u8(system);
+        let hi = self.fetch_u8(system);
+        u16::from(lo) | (u16::from(hi) << 8)
+    }
 }
 
 mod tests {
@@ -22,5 +28,19 @@ mod tests {
         let v = cpu.fetch_u8(&mut mem);
         assert_eq!(v, 0xf0u8);
         assert_eq!(cpu.pc, 0x0003u16);
+    }
+
+    # [test]
+    fn test_fetch_u16() {
+        let mut cpu = super::Cpu::default();
+        let mut mem = memory::Memory::default();
+
+        cpu.pc = 0x0002u16;
+        mem.write_u8(0x0002u16, 0x34u8);
+        mem.write_u8(0x0003u16, 0x12u8);
+
+        let v = cpu.fetch_u16(&mut mem);
+        assert_eq!(v, 0x1234u16);
+        assert_eq!(cpu.pc, 0x0004u16);
     }
 }
