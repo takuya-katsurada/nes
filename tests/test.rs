067c89fb22e3470f2c723acd1abe38910d4b4360
takuya-katsurada <116815666+takuya-katsurada@users.noreply.github.com>
2022-11-12 16:44:37 +0900

    code(nes): add tests/test.rs

---
+++ tests/test.rs
@@ -0,0 +1,20 @@
+
+mod tests {
+    use rom::Rom;
+    use std::fs::File;
+    use std::io::Read;
+
+    #[test]
+    fn load_rom() -> std::io::Result<()> {
+        let mut file = File::open("roms/nes-test-roms/other/demo.nes")?;
+        let mut contents = vec![];
+        file.read_to_end(&mut contents);
+
+        let isValid = Rom::isValid(&mut contents);
+        let rom = Rom::new(contents);
+        assert!(isValid);
+        assert_eq!(rom.prg_rom_bytes, 1);
+        assert_eq!(rom.chr_rom_bytes, 1);
+        Ok(())
+    }
+}
