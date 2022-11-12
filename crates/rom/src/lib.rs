35f0346f3abcced2d9e0f7d853edbf330e1c428a
takuya-katsurada <116815666+takuya-katsurada@users.noreply.github.com>
2022-11-12 12:17:27 +0900

    code(rom): add Rom::{new,isValid}

--- crates/rom/src/lib.rs
+++ crates/rom/src/lib.rs
@@ -1,7 +1,22 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+
+use std::io::Read;
+use std::error::Error;
+
+pub struct Rom {
+    pub prg_rom_bytes: usize,
+    pub chr_rom_bytes: usize,
+}
+
+
+impl Rom {
+    pub fn isValid(data: &mut Vec<u8>) -> bool {
+        &data[0..4] == [0x4e,0x45,0x53,0x1a]
+    }
+
+    pub fn new(data: Vec<u8>) -> Self {
+        Rom {
+            prg_rom_bytes: usize::from(data[4]),
+            chr_rom_bytes: usize::from(data[5]),
+        }
     }
 }
