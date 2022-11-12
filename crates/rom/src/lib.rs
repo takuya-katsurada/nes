4a3a98129672485fcee65637192fe3d9c74dab05
takuya-katsurada <116815666+takuya-katsurada@users.noreply.github.com>
2022-11-12 15:37:10 +0900

    code(rom): add Rom::default

--- crates/rom/src/lib.rs
+++ crates/rom/src/lib.rs
@@ -7,6 +7,14 @@ pub struct Rom {
     pub chr_rom_bytes: usize,
 }
 
+impl Default for Rom {
+    fn default() -> Self {
+        Rom {
+            prg_rom_bytes: 0,
+            chr_rom_bytes: 0,
+        }
+    }
+}
 
 impl Rom {
     pub fn isValid(data: &mut Vec<u8>) -> bool {
