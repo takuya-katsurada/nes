e6bf6d1fe298cefd34ceeda3817dba5e8f81ff16
takuya-katsurada <116815666+takuya-katsurada@users.noreply.github.com>
2022-11-13 22:03:33 +0900

    code(cpu): implement the LDY instruction

--- crates/cpu/src/lib.rs
+++ crates/cpu/src/lib.rs
@@ -118,6 +118,14 @@ impl Cpu {
                 self.x = result;
                 1 + operand.cycle
             }
+            Opcode::LDY => {
+                let operand = self.fetch(system, mode);
+                let result = operand.data;
+
+                self.check_zero_and_negative_flag(result);
+                self.y = result;
+                1 + operand.cycle
+            }
             Opcode::NOP => {
                 2
             }
@@ -429,6 +437,30 @@ mod tests {
 
             let cycle = cpu.step(&mut mem);
             assert_eq!(cpu.x, param.1);
+            assert_eq!(cpu.read_zero_flag(), param.2);
+            assert_eq!(cpu.read_negative_flag(), param.3);
+            assert_eq!(cycle, 0x02u8);
+        }
+    }
+
+    # [test]
+    fn execute_ldy_instruction()
+    {
+        let mut cpu = super::Cpu::default();
+        let mut mem = memory::Memory::default();
+
+        for param in [
+            (0x00, 0x01, false, false),
+            (0x01, 0x00, true, false),
+            (0x00, 0x80, false, true),
+        ] {
+            cpu.y  = param.0;
+            cpu.pc = 0x0000u16;
+            mem.write_u8(0x0000, 0xa0u8);
+            mem.write_u8(0x0001, param.1);
+
+            let cycle = cpu.step(&mut mem);
+            assert_eq!(cpu.y, param.1);
             assert_eq!(cpu.read_zero_flag(), param.2);
             assert_eq!(cpu.read_negative_flag(), param.3);
             assert_eq!(cycle, 0x02u8);
