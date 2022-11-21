7e372abbd0eba7033a95ae61219fa53ea92897a7
takuya-katsurada <116815666+takuya-katsurada@users.noreply.github.com>
2022-11-21 20:55:44 +0900

    code(cpu): implement the BNE instruction

--- crates/cpu/src/lib.rs
+++ crates/cpu/src/lib.rs
@@ -94,6 +94,15 @@ impl Cpu {
                     1 + operand.cycle
                 }
             }
+            Opcode::BNE => {
+                let operand = self.fetch(system, mode);
+                if !self.read_zero_flag() {
+                    self.pc = operand.address;
+                    2 + operand.cycle
+                } else {
+                    1 + operand.cycle
+                }
+            }
             Opcode::CLC => {
                 self.write_carry_flag(false);
                 2
@@ -496,6 +505,28 @@ mod tests {
             cpu.pc = 0x0000u16;
             cpu.write_zero_flag(param.0);
             mem.write_u8(0x0000, 0xf0u8);
+            mem.write_u8(0x0001, 0x10u8);
+            mem.write_u8(0x0012, 0xffu8);
+
+            let cycle = cpu.step(&mut mem);
+            assert_eq!(cpu.pc, param.1);
+            assert_eq!(cycle, param.2);
+        }
+    }
+
+    # [test]
+    fn execute_bne_instruction()
+    {
+        let mut cpu = super::Cpu::default();
+        let mut mem = memory::Memory::default();
+
+        for param in [
+            (false, 0x0012u16, 0x03u8),
+            (true, 0x0002u16, 0x02u8),
+        ] {
+            cpu.pc = 0x0000u16;
+            cpu.write_zero_flag(param.0);
+            mem.write_u8(0x0000, 0xd0u8);
             mem.write_u8(0x0001, 0x10u8);
             mem.write_u8(0x0012, 0xffu8);
 
