568b3369d01ecee6fde6f9215c23e62725229c29
takuya-katsurada <116815666+takuya-katsurada@users.noreply.github.com>
2022-11-25 23:02:17 +0900

    code(ppu): add constants

--- crates/ppu/src/lib.rs
+++ crates/ppu/src/lib.rs
@@ -1,14 +1,15 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+/// CPU cycles per line.
+pub const CPU_CYCLES_PER_LINE: usize = (341 / 3);
+
+/// Render screen area (width).
+pub const RENDER_SCREEN_AREA_WIDTH: usize = 256;
+/// Render screen area (height).
+pub const RENDER_SCREEN_AREA_HEIGHT: usize = 240;
+
+/// CPU cycles per draw frame.
+pub const CPU_CYCLES_PER_DRAW_FRAME: usize = CPU_CYCLES_PER_LINE * ((RENDER_SCREEN_AREA_HEIGHT + 1) as usize);
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
