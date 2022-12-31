cad073f3356ecb4e9cd5ff0bb80dad04c6a4f571
takuya-katsurada <116815666+takuya-katsurada@users.noreply.github.com>
2022-12-31 14:58:36 +0900

    env(example): update Cargo.toml

--- examples/nes-desktop-example-app/src/main.rs
+++ examples/nes-desktop-example-app/src/main.rs
@@ -1,3 +1,17 @@
+use clap::{Parser};
+
+#[derive(Debug, Parser)]
+#[clap(
+    name = env!("CARGO_PKG_NAME"),
+    version = env!("CARGO_PKG_VERSION"),
+    author = env!("CARGO_PKG_AUTHORS"),
+    about = env!("CARGO_PKG_DESCRIPTION"),
+    arg_required_else_help = true,
+)]
+struct Cli {
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
 }
