29244adb3d4fb9b6a44107aebae5a63570f89d23
takuya-katsurada <116815666+takuya-katsurada@users.noreply.github.com>
2022-12-31 16:34:12 +0900

    code(example): check if the given path is available

--- examples/nes-desktop-example-app/src/main.rs
+++ examples/nes-desktop-example-app/src/main.rs
@@ -1,4 +1,6 @@
 use clap::{Parser};
+use std::fs;
+use std::path::PathBuf;
 
 #[derive(Debug, Parser)]
 #[clap(
@@ -9,9 +11,18 @@ use clap::{Parser};
     arg_required_else_help = true,
 )]
 struct Cli {
-
+    /// Path of the iNES file to be set in the emulator
+    #[clap(short = 'p', long = "path", value_name = "FILE")]
+    file: String
 }
 
 fn main() {
     let cli = Cli::parse();
+
+    let path = PathBuf::from(cli.file);
+    let f = match fs::canonicalize(&path) {
+        Ok(file) => file,
+        Err(error) => panic!("{:?}", error)
+    };
+    println!("{:?}", f);
 }
